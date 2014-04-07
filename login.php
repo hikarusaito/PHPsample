38656273e3fdad237080dff7725ae94043fa2114
hikaru100ten <hikaru100ten@yahoo.co.jp>
2014-04-07 15:53:07 +0900

    ファースト

---
+++ login.php
@@ -0,0 +1,16 @@
+<!DOCTYPE HTML PUBLIC "-//W3C//DTD HTML 4.01 Transitional//EN">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8"
+<title>出席表</title>
+</head>
+<body>
+<center>
+<font color="#00000f" size="7">出席表</font><br />
+熊大ID:
+<form action="login.html" method="post">
+<input type="text" name="kuma_id" size="250px">
+<input type="submit" value="出席"><br />
+</form>
+</body>
+</html>
